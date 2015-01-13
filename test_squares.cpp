979545bb26840b58f9241161e7f07d40f9dcf547
Lisandro Jimenez <jimenezl@mit.edu>
2015-01-13 12:34:08 -0500

    fix bugs

--- test_squares.cpp
+++ test_squares.cpp
@@ -139,7 +139,7 @@ int main()
     }
 
     if (isTurning){
-      if (fabs(rf - startAngle) >= 90.0){
+      if (fabs(total - startAngle) >= 90.0){
         isTurning = false;
         setMotorSpeed(pwm, dir, -1*speed);
         setMotorSpeed(pwm2, dir2, speed);
