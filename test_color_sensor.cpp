1363b8737e92a1318fdb80e7af66eece2ba40bed
Daniel Lerner <Daniel@daniel-lerners-macbook-pro.local>
2015-01-14 17:22:22 -0500

    color sensor test 1

---
+++ test_color_sensor.cpp
@@ -0,0 +1,38 @@
+// Compile with:
+// g++ test_aio.cpp -o test_aio -lmraa
+// Repeatedly reads pin A0 and prints the result.
+
+#include <csignal>
+#include <iostream>
+
+#include "mraa.hpp"
+
+int running = 1;
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT) {
+    printf("closing spi nicely\n");
+    running = 0;
+  }
+}
+
+int main() {
+  // Handle Ctrl-C quit
+  signal(SIGINT, sig_handler);
+
+  mraa::Aio aio = mraa::Aio(0);
+
+  while (running) {
+    int val = aio.read();
+    std::cout << "Read: " << val << std::endl;
+    if (val > 315){
+      printf("Green Light")
+    }
+
+    else{
+      printf("Red Light")
+    }
+    sleep(1);
+  }
+}
