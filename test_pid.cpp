dcf491e58c5a6d076725d0d6e7074905d0884727
Lisandro Jimenez <jimenezl@mit.edu>
2015-01-13 19:03:23 -0500

    changed forwardBias to .1

--- test_pid.cpp
+++ test_pid.cpp
@@ -99,7 +99,7 @@ int main()
   float derivative = 0;
   float timeBetweenReadings = 0;
   float gyroBias = 1.0;
-  float forwardBias = .3;
+  float forwardBias = .1;
   float P_CONSTANT = 25;
   float I_CONSTANT = 0;
   float D_CONSTANT = 0;
