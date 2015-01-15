b5d25f939a496b1ed14cfc6f138f2855f43b22e9
Your Name <you@example.com>
2015-01-15 10:36:25 -0700

    sdaf

--- test_pid.cpp
+++ test_pid.cpp
@@ -40,11 +40,11 @@ void sig_handler(int signo) {
 void setMotorSpeed(mraa::Pwm &pwm, mraa::Gpio &dir, double speed) {
     
     assert(-1.0 <= speed && speed <= 1.0);
-    // if (speed > .5) {
-    //     speed = .5;
-    // } else (speed < -.5) {
-    //     speed = -.5;
-    // }
+    if (speed > .5) {
+        speed = .5;
+    } else if (speed < -.5) {
+        speed = -.5;
+    }
     if (speed < 0) {
         dir.write(1);
     } else {
