60d50eaab7520419b181070d368816e70a045e5d
Daniel Lerner <Daniel@dhcp-18-111-8-49.dyn.mit.edu>
2015-01-15 17:45:41 -0500

    fix writeReg to write

--- test_adafruit_color_sensor_real.cpp
+++ test_adafruit_color_sensor_real.cpp
@@ -43,12 +43,10 @@ void init_TCS34725(mraa::I2c *i2c) {
 	enable[0] = EnableAddress;
 	enable[1] = 0x03;
 
-
-
 	assert(0 == i2c->address(SensorAddress));  
 	mraa::printError(i2c->write(timee, 2)); 
 	assert(0 == i2c->write(gain, 2)); 
-	assert(0 == i2c->writeReg(enable, 2));
+	assert(0 == i2c->write(enable, 2));
 }
 
 void get_Colors(mraa::I2c *i2c){
