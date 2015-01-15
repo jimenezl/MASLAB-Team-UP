c7ae3aaad30d57965cdbf57e59339db20d82e514
Daniel Lerner <Daniel@dhcp-18-111-8-49.dyn.mit.edu>
2015-01-15 17:31:07 -0500

    print error

--- test_adafruit_color_sensor_real.cpp
+++ test_adafruit_color_sensor_real.cpp
@@ -33,7 +33,7 @@ void init_TCS34725(mraa::I2c *i2c) {
 	
 	
 	assert(0 == i2c->address(SensorAddress));  
-	std::cout<< i2c->writeReg(ATimeAddress, 0xf6)<<std::endl; 
+	mraa::printError(i2c->writeReg(ATimeAddress, 0xf6)); 
 	assert(0 == i2c->writeReg(ControlAddress, 0x00)); 
 	assert(0 == i2c->writeReg(EnableAddress, 0x03));
 }
