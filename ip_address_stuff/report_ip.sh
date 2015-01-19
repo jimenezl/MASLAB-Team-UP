d7fb4fa4a3f71674343dbefbd96de743eb55e4be
Your Name <you@example.com>
2015-01-19 02:55:49 -0700

    got ip address reporting to work

--- ip_address_stuff/report_ip.sh
+++ ip_address_stuff/report_ip.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# echo "hi"
+hello="hello world"
+ip_address=`ifconfig $1 | grep "inet addr:18." | awk -F: '{print $2}' | awk '{print $1}'`
+echo "edison IP:" + "$ip_address" > ipaddress.txt
 
-ip_address=ifconfig $1 | grep "inet addr:18." | awk -F: '{print $2}' | awk '{print $1}'
-curl --data "ipaddress=$ip_address" team9up.xvm.mit.edu/report-ip.php
+# function int-ip { ifconfig $1 | grep "inet addr:18." | awk -F: '{print $2}' | awk '{print $1}'; }
+# echo int-ip
+
+# curl --data "ipaddress=$ip_address" teamup.xvm.mit.edu/report-ip.php
+
+curl --url "smtps://smtp.gmail.com:465" --ssl-reqd \
+  --mail-from "sandroraspi@gmail.com" --mail-rcpt "sandro1321@gmail.com" \
+  --upload-file ipaddress.txt --user "sandroraspi@gmail.com:trumpet13" --insecure
