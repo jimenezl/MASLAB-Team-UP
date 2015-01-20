1b56c3c7b5f4a1325e2693fc569204835721c748
Daniel Lerner <Daniel@daniel-lerners-macbook-pro.local>
2015-01-19 20:34:08 -0500

    test servo shield with dc motor

---
+++ test_drive_motor_servo_shield.cpp
@@ -0,0 +1,132 @@
+// Compile with:
+// g++ test_drive_motor_servo_shield.cpp -o test_drive -lmraa
+// Controls a servo on port 0 of the servo shield.
+
+#include "mraa.hpp"
+#include <cassert>
+#include <csignal>
+#include <iostream>
+
+#define SHIELD_I2C_ADDR 0x40
+
+int running = 1;
+#define MS 1000
+
+uint8_t registers[] = {
+    6,   // output 0
+    10,  // output 1
+    14,  // output 2
+    18,  // output 3
+    22,  // output 4
+    26,  // output 5
+    30,  // output 6
+    34,  // output 7
+    38,  // output 8
+    42,  // output 9
+    46,  // output 10
+    50,  // output 11
+    54,  // output 12
+    58,  // output 13
+    62,  // output 14
+    66   // output 15
+};
+
+
+void sig_handler(int signo) {
+    if (signo == SIGINT) {
+        printf("closing spi nicely\n");
+        running = 0;
+    }
+}
+
+void initPWM(mraa::I2c *i2c) {
+    char writeBuf[2] = {0};
+    writeBuf[0] = 0x00; // Write to MODE 1 Register;
+    writeBuf[1] = 1 << 4; // Enable Sleep Mode
+
+    i2c->address(SHIELD_I2C_ADDR);
+    i2c->write(writeBuf, 2);
+
+    usleep(10 * MS);
+
+    writeBuf[0] = 0xFE; // Write Prescaler Register
+    writeBuf[1] = 0xA3; // Set pwm frequency to ~40 Hz
+
+    i2c->address(SHIELD_I2C_ADDR);
+    i2c->write(writeBuf, 2);
+
+    writeBuf[0] = 0; // Write to the MODE 1 register
+    writeBuf[1] = 1 << 5 // Enable auto increment mode
+                  | 0 << 4; // Enable the oscillator
+
+    i2c->address(SHIELD_I2C_ADDR);
+    i2c->write(writeBuf, 2);
+}
+
+
+void writePWM(mraa::I2c *i2c, int index, double duty) {
+    assert(0.0 <= duty && duty <= 1.0);
+    assert(0 <= index && index < 16);
+    double on = 4095.0 * duty;
+
+    uint16_t onRounded = (uint16_t) on;
+
+    char writeBuf[5];
+
+    // ON_L
+    writeBuf[0] = registers[index];
+    writeBuf[1] = 0x00; // ON LSB
+    writeBuf[2] = 0x00; // ON MSB
+    writeBuf[3] = onRounded & 0xFF; // OFF LSB
+    writeBuf[4] = (onRounded >> 8) & 0xFF; // OFF MSB
+    i2c->address(SHIELD_I2C_ADDR);
+    i2c->write(writeBuf, 5);
+}
+
+void setMotorSpeed(mraa::Pwm &pwm, mraa::Gpio &dir, double speed) {
+    assert(-1.0 <= speed && speed <= 1.0);
+    if (speed < 0) {
+        dir.write(1);
+    } else {
+        dir.write(0);
+    }
+    pwm.write(fabs(speed));
+}
+
+
+void setServoPosition(mraa::I2c *i2c, int index, double duty) {
+    writePWM(i2c, index, .04 * duty + .04);
+}
+
+int main() {
+    // Handle Ctrl-C quit
+    signal(SIGINT, sig_handler);
+
+    //analog readings
+    mraa::Aio aio = mraa::Aio(2);
+
+    // Edison i2c bus is 6
+    mraa::I2c *i2c = new mraa::I2c(6);
+    assert(i2c != NULL);
+
+    initPWM(i2c);
+
+    while (running) {
+
+        int val = aio.read();
+        std::cout << "Read: " << val << std::endl;
+        // Alternate two locations with 2-sec delay
+        /*
+        std::cout << "Speed: " << speed << std::endl;
+        setMotorSpeed(pwm, dir, speed);
+        setMotorSpeed(pwm2, dir2, -1 * speed);
+
+        if (speed > 1.0) {
+            speed = -1.0;
+            // Let the motor spin down
+            setMotorSpeed(pwm, dir, 0.0);
+            setMotorSpeed(pwm2, dir2, 0.0);
+            sleep(2.0);
+            */
+    }
+}
