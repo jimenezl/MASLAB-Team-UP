9c9e54fcc65d35991f1ac4f445d79438160f1bba
Your Name <you@example.com>
2015-01-23 17:03:51 -0500

    added p constant

--- rgbObjectTrackingAndFollowing.cpp
+++ rgbObjectTrackingAndFollowing.cpp
@@ -654,7 +654,7 @@ int main() {
         diffAngle = desiredAngle - total;
         integral += diffAngle * 0.001 * timeBetweenReadings;
         derivative = (rf / 80.0);
-        power = speed * ((P_CONSTANT * diffAngle / 360.0));// + (I_CONSTANT * integral) + (D_CONSTANT * derivative / 180.0)); //make sure to convert angles > 360 to proper angles
+        power = speed * ((P_CONSTANT * diffAngle / 360.0)) + (I_CONSTANT * integral) + (D_CONSTANT * derivative / 180.0)); //make sure to convert angles > 360 to proper angles
 
         if (power > .2) {
             power = .2;
