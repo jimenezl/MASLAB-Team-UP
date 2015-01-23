9c5db6ea748313853a1f5fd2c711e37e8fd02012
Your Name <you@example.com>
2015-01-23 17:30:32 -0500

    trying to get i term to work

--- test_pid.cpp
+++ test_pid.cpp
@@ -98,7 +98,7 @@ int main() {
     float gyroBias = 1.0;
     float forwardBias = .1;
     float P_CONSTANT = 25;
-    float I_CONSTANT = 0;
+    float I_CONSTANT = .1;
     float D_CONSTANT = -1;
 
     while (running) {
@@ -147,7 +147,7 @@ int main() {
         }
 
         diffAngle = desiredAngle - total;
-        integral += diffAngle * 0.001 * timeBetweenReadings;
+        integral = integral*.9 + (diffAngle * 0.001 * timeBetweenReadings);
         derivative = (rf / 80.0);
         power = speed * ((P_CONSTANT * diffAngle / 360.0) + (I_CONSTANT * integral) + (D_CONSTANT * derivative / 180.0)); //make sure to convert angles > 360 to proper angles
 
