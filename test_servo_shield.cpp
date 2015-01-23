0624d94025d9cae9a6a8da1ffc7eb42a9b2288b0
Daniel Lerner <Daniel@daniel-lerners-macbook-pro.local>
2015-01-23 18:49:16 -0500

    gripper range additiong

--- test_servo_shield.cpp
+++ test_servo_shield.cpp
@@ -103,7 +103,7 @@ int main()
     // Alternate two locations with 2-sec delay
     setServoPosition(i2c, 0, 1.2);  // open// -0.2 to 1.4 max with servo head parallel to servo
     sleep(2.0); 
-    setServoPosition(i2c, 0, 0.4); // close
+    setServoPosition(i2c, 0, 0.5); // close
     sleep(2.0);
 
   }
