48782007ba8eeee7fdfb3608dd77f6878e3554d0
Your Name <you@example.com>
2015-01-23 20:13:52 -0500

    lowering angle threshold

--- rgbObjectTrackingAndFollowing.cpp
+++ rgbObjectTrackingAndFollowing.cpp
@@ -660,7 +660,7 @@ int main() {
         derivative = (rf / 80.0);
         power = speed * ((P_CONSTANT * diffAngle / 360.0) + (I_CONSTANT * integral) + (D_CONSTANT * derivative / 180.0)); //make sure to convert angles > 360 to proper angles
         
-        if (diffAngle<10){
+        if (diffAngle<5){
 	        forwardBias = -.1 * distanceToBlock;
 	    } else {
 	    	forwardBias = 0.0;
