902234fe029d14590c9105d954604bd89f4cf198
Your Name <jimenezl@mit.edu>
2015-01-27 18:25:57 -0500

    added extra pin reading

--- test_infrared_lerner.cpp
+++ test_infrared_lerner.cpp
@@ -21,11 +21,14 @@ int main() {
   // Handle Ctrl-C quit
   signal(SIGINT, sig_handler);
 
-  mraa::Aio aio = mraa::Aio(2);
+  mraa::Aio aioPinTwo = mraa::Aio(2);
+  mraa::Aio aioPintThree = mraa::Aio(3);
 
   while (running) {
-    int val = aio.read();
-    std::cout << "Read: " << val << std::endl;
+    int val1 = aioPinTwo.read();
+    int val2 = aioPintThree.read();
+    printf("Pin two: %d, pin three: %d\n", val1, val2);
+    // std::cout << "Read: " << val << std::endl;
     sleep(1);
 
 
