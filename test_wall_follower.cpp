7676ff77301e7cf004e689f34d21f65335dc9713
Your Name <jimenezl@mit.edu>
2015-01-27 03:48:17 -0500

    changed ir sensor pins

--- test_wall_follower.cpp
+++ test_wall_follower.cpp
@@ -30,8 +30,8 @@ float DISTANCE_FROM_IR_SENSORS = 5.1;
 // float QUAD_TERM = ;
 // float LINEAR_TERM = ;
 // float CONST_TERM = ;
-BACK_INFRARED_PIN = ;
-FRONT_INFRARED_PIN = ;
+BACK_INFRARED_PIN = 1;
+FRONT_INFRARED_PIN = 0;
 
 int running = 1;
 
