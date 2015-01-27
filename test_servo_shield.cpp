a2416d31980d9398e6098d190973a52bb1368e7a
Daniel Lerner <Daniel@daniel-lerners-macbook-pro.local>
2015-01-27 02:38:36 -0500

    servo range test for door

--- test_servo_shield.cpp
+++ test_servo_shield.cpp
@@ -103,7 +103,7 @@ int main()
     // Alternate two locations with 2-sec delay
     setServoPosition(i2c, 3, 0.0);  // open// -0.2 to 1.4 max with servo head parallel to servo
     sleep(2.0); 
-    setServoPosition(i2c, 3, 0.5); // close
+    setServoPosition(i2c, 3, 1.0); // close
     sleep(2.0);
   }
 }
