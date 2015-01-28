6d2d0ed84be0cb3a84fd3794e4b8f21b884b6e12
Your Name <jimenezl@mit.edu>
2015-01-28 01:04:09 -0500

    added usleeps

--- test_wall_follower.cpp
+++ test_wall_follower.cpp
@@ -152,7 +152,7 @@ int main() {
     float derivative = 0;
     float timeBetweenReadings = 0;
     float gyroBias = 1.0;
-    float forwardBias = 0.1;
+    float forwardBias = 0;
     float P_CONSTANT = 25;
     float I_CONSTANT = 0;
     float D_CONSTANT = -1;
@@ -188,6 +188,7 @@ int main() {
         setMotorSpeed(pwm, dir, power - forwardBias);
         setMotorSpeed(pwm2, dir2, power + forwardBias);
         printf("Set power to: %f\n", power);
+        usleep(1000 * 10);
 
     }
 
