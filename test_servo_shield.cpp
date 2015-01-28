51247ec1c7f7e6c302d3e2e7d65bc7c2effad392
Daniel Lerner <Daniel@dhcp-18-111-11-95.dyn.mit.edu>
2015-01-28 05:01:43 -0500

    arm test

--- test_servo_shield.cpp
+++ test_servo_shield.cpp
@@ -101,10 +101,10 @@ int main()
 
   while (running) {
     // Alternate two locations with 2-sec delay
-    setServoPosition(i2c, 0, 1.0);  // perpendicular to brackets  -0.2 to 1.4 max with servo head parallel to servo
+    setServoPosition(i2c, 0, 0.8);  // perpendicular to brackets  -0.2 to 1.4 max with servo head parallel to servo
     sleep(2.0);
 
-  setServoPosition(i2c, 0, 0.0); // close
+    setServoPosition(i2c, 0, 0.2); // close
     sleep(2.0);
     
     
