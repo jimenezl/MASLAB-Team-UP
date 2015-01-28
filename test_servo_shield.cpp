3c2a80125286bde50695e6b7dfd5ef505f91fd14
Daniel Lerner <Daniel@dhcp-18-111-11-95.dyn.mit.edu>
2015-01-28 05:10:45 -0500

    servo range test

--- test_servo_shield.cpp
+++ test_servo_shield.cpp
@@ -101,7 +101,7 @@ int main()
 
   while (running) {
     // Alternate two locations with 2-sec delay
-    setServoPosition(i2c, 0, 1.5);  // perpendicular to brackets  -0.2 to 1.4 max with servo head parallel to servo
+    setServoPosition(i2c, 0, 1.2);  // perpendicular to brackets  -0.2 to 1.4 max with servo head parallel to servo
     sleep(2.0);
 
    // setServoPosition(i2c, 0, 0.2); // close
