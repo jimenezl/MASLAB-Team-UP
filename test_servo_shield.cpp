f21946eb277b53b1c5e70cd4be75d4abb5e2dd4c
Daniel Lerner <Daniel@dhcp-18-111-55-207.dyn.mit.edu>
2015-01-28 17:19:09 -0500

    servo test

--- test_servo_shield.cpp
+++ test_servo_shield.cpp
@@ -100,7 +100,7 @@ int main()
 
   while (running) {
     // Alternate two locations with 2-sec delay
-    setServoPosition(i2c, 0, 1.5);  // 1.5 fully open 
+    setServoPosition(i2c, 0, 0);  // 1.5 fully open 
     printf("(Position at 1.3)\n");
     sleep(2.0);
     //setServoPosition(i2c, 0, 0.70);
