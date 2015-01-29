d35d50e206313279058136bed0c21e1285d529ba
Your Name <jimenezl@mit.edu>
2015-01-29 05:39:33 -0500

    increased p term

--- rgbObjectTrackingAndFollowingBufferFix.cpp
+++ rgbObjectTrackingAndFollowingBufferFix.cpp
@@ -622,7 +622,7 @@ int main() {
 
     float previousDistance = 0;
 
-    float P_CONSTANT_DISTANCE = .0015;
+    float P_CONSTANT_DISTANCE = .002;
     float D_CONSTANT_DISTANCE = .0002;
 
     while (running) {
