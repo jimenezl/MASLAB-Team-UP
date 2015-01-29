60b74dfcd2ad4012390194439d5b08b175e1407c
Daniel Lerner <Daniel@dhcp-18-111-20-150.dyn.mit.edu>
2015-01-29 15:59:06 -0500

    servo range test for gripper pin switch

--- test_servo_shield.cpp
+++ test_servo_shield.cpp
@@ -101,10 +101,10 @@ int main()
 
   while (running) {
     // Alternate two locations with 2-sec delay
-    setServoPosition(i2c, 4, 1.6);  // 1.5 fully open 
+    setServoPosition(i2c, 0, 1.6);  // 1.5 fully open 
 
     sleep(2.0);
-    setServoPosition(i2c, 4, 1.1);
+    setServoPosition(i2c, 0, 1.1);
 
     sleep(2.0);
   }
