b7dfba2f82173890c3286d64e837648c11e991ae
Your Name <jimenezl@mit.edu>
2015-01-29 04:43:44 -0500

    changed bias p constant

--- rgbObjectTrackingAndFollowingBufferFix.cpp
+++ rgbObjectTrackingAndFollowingBufferFix.cpp
@@ -678,7 +678,7 @@ int main() {
         power = speed * ((P_CONSTANT * diffAngle / 360.0) + (I_CONSTANT * integral) + (D_CONSTANT * derivative / 180.0)); //make sure to convert angles > 360 to proper angles
         
         if (diffAngle<5){
-	        forwardBias = .005 * distanceToBlock;
+	        forwardBias = .002 * distanceToBlock;
 	        // forwardBias = 0.0;
 	    } else {
 	    	forwardBias = 0.0;
