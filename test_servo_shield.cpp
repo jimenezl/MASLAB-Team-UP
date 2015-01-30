5dbe6ac45802b9395eb0a640295c9818ba94edb2
Daniel Lerner <Daniel@daniel-lerners-macbook-pro.local>
2015-01-30 08:16:55 -0500

    -1 to 1 servo test

--- test_servo_shield.cpp
+++ test_servo_shield.cpp
@@ -102,9 +102,9 @@ int main()
   while (running) {
     // Alternate two locations with 2-sec delay
 
-    setServoPosition(i2c, 0, 0.25);
+    setServoPosition(i2c, 0, -1);
     sleep(2.0);
-    setServoPosition(i2c, 0, 0.90);
+    setServoPosition(i2c, 0, 1);
     sleep(2.0);
   }
 }
