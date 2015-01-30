c4657dfb7273cb584066566334fe9167c5c80bd9
Your Name <jimenezl@mit.edu>
2015-01-30 16:44:35 -0500

    increased power

--- robot_state_machine.cpp
+++ robot_state_machine.cpp
@@ -917,7 +917,7 @@ int main() {
     mraa::Aio aioFrontInfrared = mraa::Aio(FRONT_INFRARED_PIN);
     mraa::Aio aioHeadInfrared = mraa::Aio(HEAD_INFRARED_PIN);
 
-    float speed = .14;
+    float speed = .15;
     // float desiredAngle = 0.0; //making this global so camera thread can use and change it
     float diffAngle = 0.0;
     float integral = 0;
@@ -938,7 +938,7 @@ int main() {
     
 
     //wall follower constants
-    float speedWallFollower = .14;
+    float speedWallFollower = .15;
     float powerWallFollower = 0;
     float forwardBiasWallFollower = .15;
 
