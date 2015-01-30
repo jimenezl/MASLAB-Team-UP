593561880e1ca1231f55482ad155ba15a5042f45
Daniel Lerner <Daniel@daniel-lerners-macbook-pro.local>
2015-01-30 08:08:46 -0500

    servo opens

--- test_servo_shield.cpp
+++ test_servo_shield.cpp
@@ -102,9 +102,9 @@ int main()
   while (running) {
     // Alternate two locations with 2-sec delay
 
-    setServoPosition(i2c, 0, 0.90);
-    sleep(2.0);
     setServoPosition(i2c, 0, 0.25);
+    sleep(2.0);
+    setServoPosition(i2c, 0, 0.90);
     sleep(2.0);
   }
 }
